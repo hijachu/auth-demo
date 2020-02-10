750cb4e9b8496ae19596a3ad8be6d7bde5a088ec
YuChih Hu <yuchih@honeywld.com.tw>
2020-02-10 09:51:16 +0800

    Project init and refine code.

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -2,14 +2,15 @@ import { Http, BaseRequestOptions, Response, ResponseOptions, RequestMethod } fr
 import { MockBackend, MockConnection } from '@angular/http/testing';
 
 export function fakeBackendFactory(
-    backend: MockBackend, 
-    options: BaseRequestOptions) {
-        
+  backend: MockBackend,
+  options: BaseRequestOptions) {
+
+  // tslint:disable-next-line:max-line-length
   let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1vc2ggSGFtZWRhbmkiLCJhZG1pbiI6dHJ1ZX0.iy8az1ZDe-_hS8GLDKsQKgPHvWpHl0zkQBqy1QIPOkA';
-    
+
   backend.connections.subscribe((connection: MockConnection) => {
-    // We are using the setTimeout() function to simulate an 
-    // asynchronous call to the server that takes 1 second. 
+    // We are using the setTimeout() function to simulate an
+    // asynchronous call to the server that takes 1 second.
     setTimeout(() => {
       //
       // Fake implementation of /api/authenticate
@@ -23,42 +24,39 @@ export function fakeBackendFactory(
             new ResponseOptions({
               status: 200,
               body: { token: token }
-           })));
+            })));
         } else {
           connection.mockRespond(new Response(
-            new ResponseOptions({ status: 200 })
+            new ResponseOptions({ status: 200 }) // 400 is better (invalid user)
           ));
         }
       }
 
-
-
-       // 
-       // Fake implementation of /api/orders
-       //
-       if (connection.request.url.endsWith('/api/orders') && 
-           connection.request.method === RequestMethod.Get) {
-         if (connection.request.headers.get('Authorization') === 'Bearer ' + token) {
-            connection.mockRespond(new Response(
-              new ResponseOptions({ status: 200, body: [1, 2, 3] })
-         ));
-       } else {
-           connection.mockRespond(new Response(
-             new ResponseOptions({ status: 401 })
-           ));
-       }
-    }
-
-
+      //
+      // Fake implementation of /api/orders
+      //
+      if (connection.request.url.endsWith('/api/orders') &&
+        connection.request.method === RequestMethod.Get) {
+        if (connection.request.headers.get('Authorization') === 'Bearer ' + token) {
+          connection.mockRespond(new Response(
+            new ResponseOptions({ status: 200, body: [1, 2, 3] })
+          ));
+        } else {
+          connection.mockRespond(new Response(
+            new ResponseOptions({ status: 401 })  // unauthorized
+          ));
+        }
+      }
 
     }, 1000);
+
   });
 
   return new Http(backend, options);
 }
 
 export let fakeBackendProvider = {
-    provide: Http,
-    useFactory: fakeBackendFactory,
-    deps: [MockBackend, BaseRequestOptions]
+  provide: Http,
+  useFactory: fakeBackendFactory,
+  deps: [MockBackend, BaseRequestOptions]
 };
